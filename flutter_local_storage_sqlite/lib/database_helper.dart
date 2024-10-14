bf5c3b2837d6e50758b3bbaf70bc7f3193762aa1
PoojithaBurugupalli <pburugupalli1@gmail.com>
2024-10-14 19:09:34 -0400

    Added files

---
+++ flutter_local_storage_sqlite/lib/database_helper.dart
@@ -0,0 +1,67 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+
+class DatabaseHelper {
+  static const _databaseName = "MyDatabase.db";
+  static const _databaseVersion = 1;
+
+  static const table = 'my_table';
+  static const columnId = '_id';
+  static const columnName = 'name';
+  static const columnAge = 'age';
+
+  late Database _db;
+
+  Future<void> init() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, _databaseName);
+    _db = await openDatabase(
+      path,
+      version: _databaseVersion,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE $table (
+        $columnId INTEGER PRIMARY KEY,
+        $columnName TEXT NOT NULL,
+        $columnAge INTEGER NOT NULL
+      )
+    ''');
+  }
+
+  Future<int> insert(Map<String, dynamic> row) async {
+    return await _db.insert(table, row);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAllRows() async {
+    return await _db.query(table);
+  }
+
+  Future<int> update(Map<String, dynamic> row) async {
+    int id = row[columnId];
+    return await _db.update(
+      table,
+      row,
+      where: '$columnId = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<int> delete(int id) async {
+    return await _db.delete(
+      table,
+      where: '$columnId = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<int> queryRowCount() async {
+    final results = await _db.rawQuery('SELECT COUNT(*) FROM $table');
+    return Sqflite.firstIntValue(results) ?? 0;
+  }
+}
